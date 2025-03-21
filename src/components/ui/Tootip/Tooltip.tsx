859acc338d7a562eb26a8e74e50a3f40d8edc8b0
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-03-21 08:54:16 +0700

    update: component button with tooltip options

--- src/components/ui/Tootip/Tooltip.tsx
+++ src/components/ui/Tootip/Tooltip.tsx
@@ -11,14 +11,14 @@ export default function Tooltip({
   tooltipText = "Tooltip",
   position = "top",
 }: ITooltipProps) {
-  const tooltipPositionClasses = {
+  const tooltipPositionClass = {
     top: "bottom-full left-1/2 -translate-x-1/2 mb-2",
     bottom: "top-full left-1/2 -translate-x-1/2 mt-2",
     left: "right-full top-1/2 -translate-y-1/2 mr-2",
     right: "left-full top-1/2 -translate-y-1/2 ml-2",
   };
 
-  const positionClasses = {
+  const positionClass = {
     top: "absolute top-full left-1/2 -translate-x-1/2 border-4 border-transparent border-t-gray-900 dark:border-t-gray-700",
     bottom:
       "absolute bottom-full left-1/2 -translate-x-1/2 border-4 border-transparent border-b-gray-900 dark:border-b-gray-700",
@@ -31,11 +31,11 @@ export default function Tooltip({
     <div
       className={cn(
         "absolute min-w-24 px-3 py-2 text-sm font-medium text-white bg-pink-600 rounded-md shadow-sm transition-all duration-300 z-50",
-        tooltipPositionClasses[position]
+        tooltipPositionClass[position]
       )}
     >
       {tooltipText}
-      <div className={positionClasses[position]}></div>
+      <div className={positionClass[position]}></div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,29 +16,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start rounded-sm">
         {/* Start Component */}
-        <Button
-          // startContent={<CiHeart size={30} className="text-pink-400" />}
-          // endContent={<CiMinimize1 size={30} className="text-pink-400" />}
-          bgColor="danger"
-          // variant="bordered"
-          radius="full"
-          size="lg"
-          tooltipText="Try using Tab to focus on the buttons as well"
-          tooltipPosition="bottom"
-          // isLoading
-          // isIconOnly
-          // spinnerPlacement="end"
-          // spinner={
-          //   <RingLoader
-          //     color="#FFFFFF"
-          //     loading={true}
-          //     className="block my-0 mx-auto border-slate-100"
-          //     size={22}
-          //     aria-label="Loading Spinner"
-          //     data-testid="loader"
-          //   />
-          // }
-        >
+        <Button size="lg" radius="full" bgColor="danger">
           Save
         </Button>
         {/* End Component */}
